eb3f6259facdf1f04e14c8caffa02f21f0f43420
Johannes Millan <johannes.millan@gmail.com>
2017-04-16 03:36:29 +0200

    make resizing work

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const exec = require('child_process').exec;
+const spawn = require('child_process').spawn;
+const execFile = require('child_process').execFile;
 const Store = require('jfs');
 const db = new Store('sessionStore');
+const WAIT_FOR_PROGRAMS_TO_START_TIMEOUT = 1000;
 const executableFileMap = {
   'sun-awt-X11-XFramePeer.jetbrains-idea': 'jetbrains-idea.desktop',
   'gnome-terminal-server.Gnome-terminal': 'gnome-terminal',
@@ -9,6 +12,7 @@ const executableFileMap = {
 const executableFileExclusions = [
   'N/A',
   'desktop_window.Nautilus',
+  'tilda.Tilda',
 ];
 
 function savePositions(sessionName) {
@@ -20,9 +24,17 @@ function savePositions(sessionName) {
 }
 
 function startSession(sessionName) {
-  db.get(sessionName || 'DEFAULT', (err, windowList) => {
+  db.get(sessionName || 'DEFAULT', (err, savedWindowList) => {
     getActiveWindowList((currentWindowList) => {
-      startSessionPrograms(windowList, currentWindowList);
+      startSessionPrograms(savedWindowList, currentWindowList);
+
+      // TODO make this smarter by actively polling the opened windows
+      setTimeout(() => {
+        updateWindowIds(savedWindowList, currentWindowList);
+        restoreWindowPositions(savedWindowList, () => {
+          console.log('RESTORE SESSION');
+        });
+      }, WAIT_FOR_PROGRAMS_TO_START_TIMEOUT);
     });
   });
 }
@@ -58,6 +70,7 @@ function setFilePath(win) {
   });
 }
 
+// TODO check for how many instances there should be running of a program
 function startSessionPrograms(windowList, currentWindowList) {
   windowList.forEach((win) => {
     if (!isProgramAlreadyRunning(win.executableFile, currentWindowList)) {
@@ -101,15 +114,17 @@ function isExcluded(executableFile) {
 function startProgram(executableFile, desktopFilePath) {
   let cmd;
   if (desktopFilePath) {
-    cmd = `awk '/^Exec=/ {sub("^Exec=", ""); gsub(" ?%[cDdFfikmNnUuv]", ""); exit system($0)}' ${executableFile}`;
+    cmd = `awk '/^Exec=/ {sub("^Exec=", ""); gsub(" ?%[cDdFfikmNnUuv]", ""); exit system($0)}' ${desktopFilePath}`;
   } else {
     cmd = executableFile;
   }
-  exec(cmd, (error, stdout, stderr) => {
+  exec(cmd, {
+    detached: true,
+  }, (error, stdout, stderr) => {
     if (error || stderr) {
       console.log(error, stderr);
     }
-  });
+  }).unref();
 }
 
 function transformWmctrlList(stdout) {
@@ -121,10 +136,11 @@ function transformWmctrlList(stdout) {
     if (fields && !isExcluded(fields[8])) {
       data.push({
         windowId: fields[1],
-        left: parseInt(fields[4], 10),
-        top: parseInt(fields[5], 10),
-        bottom: parseInt(fields[6], 10),
-        right: parseInt(fields[7], 10),
+        gravity: parseInt(fields[2], 10),
+        x: parseInt(fields[4], 10),
+        y: parseInt(fields[5], 10),
+        width: parseInt(fields[6], 10),
+        height: parseInt(fields[7], 10),
         executableFile: handleDesktopFiles(fields[8]),
       });
     }
@@ -141,5 +157,56 @@ function handleDesktopFiles(executableFileString) {
   }
 }
 
-//savePositions();
-startSession();
+function updateWindowIds(savedWindowList, currentWindowList) {
+  savedWindowList.forEach((win) => {
+    win.windowId = getWindowIdByExecutableFileString(win, currentWindowList);
+  });
+}
+
+function getWindowIdByExecutableFileString(win, currentWindowList) {
+  const currentWindow = currentWindowList.find((winFromCurrent) => win.executableFile === winFromCurrent.executableFile);
+  return currentWindow && currentWindow.windowId;
+}
+
+function restoreWindowPositions(savedWindowList, cb) {
+  const promises = [];
+  savedWindowList.forEach((win) => {
+    if (win.windowId) {
+      promises.push(restoreWindowPosition(win));
+    }
+  });
+
+  Promise.all(promises).then((results) => {
+    cb(results);
+  });
+}
+
+function restoreWindowPosition(win) {
+  const newPositionStr = `${win.gravity},${win.x},${win.y},${win.width},${win.height}`;
+  const states = 'remove,maximized_vert,maximized_horz,fullscreen,above,below';
+  const cmd = `wmctrl -i -r ${win.windowId} -e ${newPositionStr}  -b ${states}`;
+  console.log(cmd, win.executableFile);
+
+  //console.log(win.windowId, win.executableFile, newPositionStr);
+
+  return new Promise(function (fulfill, reject) {
+    exec(cmd, (error, stdout, stderr) => {
+      if (error || stderr) {
+        console.log(error, stderr);
+        reject(error || stderr);
+      } else {
+        console.log(stdout);
+        const lines = stdout.split('\n');
+        win.desktopFilePath = lines[0];
+        fulfill(stdout);
+      }
+    });
+  });
+}
+console.log(process.argv);
+
+if (process.argv[2] === 'save') {
+  savePositions();
+} else {
+  startSession();
+}
